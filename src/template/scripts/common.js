53e8993ae6fe2a468c670ef5a3f7730d04fb1d3a
e5ash <io@e5ash.com>
2018-01-23 07:06:36 +0300

    100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,5 +1,36 @@
 $(document).ready(function($) {
 
-	$('.text_phone').mask('+7 (999) 999-99-99');
+	$('.input_phone').mask('+7 (999) 999-99-99');
 	
+	$('.popup__title a').click(function(event) {
+		parent.jQuery.fancybox.getInstance().close();
+	});
+
+
+
+	var visible = true;
+
+
+	$('.popup_order .popup__close').click(function(event) {
+		if (visible == true) {
+			$.fancybox.open({src  : '#popup-gift',type : 'inline'});
+		}
+		visible = false;
+	});
+
+
+	$(window).on('load, scroll', function () {
+		var windowScroll = $(window).scrollTop(),
+				windowWidth = $(window).width();
+		
+		if (windowWidth > 1095) {
+			if (windowScroll > 181) {
+				$('.nav_top').addClass('nav_fix');
+			} else {
+				$('.nav_top').removeClass('nav_fix');
+			}
+		} else{
+			$('.nav_top').removeClass('nav_fix');
+		}
+	})
 });
